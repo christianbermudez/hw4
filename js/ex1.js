87e3a3dd35a2e7d3e8e5bf3433dc93852b328f14
Christian Bermudez <chrisb7560@gmail.com>
2026-02-18 22:23:52 -0800

    Creation of the intial code for homework 4

---
+++ js/ex1.js
@@ -0,0 +1,5 @@
+/* Homework 4
+  Example 1 JavaScript code
+*/
+
+console.log("Christian's Output from Homework 4 Example 1")
---
+++ js/ex2.js
@@ -0,0 +1,5 @@
+/* Homework 4
+  Example 2 JavaScript code
+*/
+
+console.log("Christian's Output from Homework 4 Example 2")
